7672d9248a3de873ac07e92fd4c92b8216a1ca42
greedjar74 <104611986+greedjar74@users.noreply.github.com>
2022-08-25 15:30:08 +0900

    RegisterViewController 주석처리

--- study/RegisterViewController.swift
+++ study/RegisterViewController.swift
@@ -33,22 +33,26 @@ class RegisterViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     let mournerList = ["1", "2", "3", "4", "5"]
     let infoList = ["1", "2", "3", "4"]
     
+    // pickerView를 위한 picker변수 생성
     let picker1 = UIPickerView()
     let picker2 = UIPickerView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 각각의 설정이 구현될 수 있도록 viewDidLoad에 넣어준다.
         self.configureContentsTextView()
         self.configureButtonSetting()
         self.configurePickerView()
         self.configureDateTimePickViewSetting()
     }
     
+    // 관계버튼의 형태 설정
     func configureButtonSetting() {
         self.button.layer.cornerRadius = 8.0
     }
     
+    // 상주말씀이 표현되는 view를 설정
     func configureContentsTextView() {
         let borderColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1.0)
         self.contentsTextField.layer.borderColor = borderColor.cgColor
@@ -56,22 +60,29 @@ class RegisterViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
         self.contentsTextField.layer.cornerRadius = 5.0
     }
     
+    // pickerView생성
     func configurePickerView() {
+        // 각각의 pickerView에 dataSource와 delegate를 연결한다.
         self.picker1.dataSource = self
         self.picker1.delegate = self
         
         self.picker2.dataSource = self
         self.picker2.delegate = self
         
+        // toolBar를 위한 toolBar변수 생성
         let toolBar = UIToolbar()
-        toolBar.sizeToFit()
+        toolBar.sizeToFit() // 화면의 좌우 사이즈에 딱 맞도록 설정
         
+        // toolBar에 추가될 각각의 버튼 생성
+        // toolBar에 들어가는 버튼에 함수를 추가할 땐 selector를 사용한다.
         let btnDone = UIBarButtonItem(title: "확인", style: .done, target: self, action: #selector(onPickDone))
         let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
         let btnCancel = UIBarButtonItem(title: "취소", style: .done, target: self, action: #selector(onPickCancel))
+        // toolBar에 각각의 버튼 추가
         toolBar.setItems([btnCancel, space, btnDone], animated: true)
-        toolBar.isUserInteractionEnabled = true
+        toolBar.isUserInteractionEnabled = true // 사용자가 상호작용할 수 있도록 설정
         
+        // textField에 각각의 picker를 연결, toolBar를 넣어준다.
         self.mournerTextField.inputView = picker1
         self.mournerTextField.inputAccessoryView = toolBar
         
@@ -79,7 +90,9 @@ class RegisterViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
         self.infoTextField.inputAccessoryView = toolBar
     }
     
+    // 날짜를 설정하는 DatePickerView를 설정
     func configureDateTimePickViewSetting() {
+        // 테두리 색상을 미리 변수로 만들어둔다.
         let borderColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1.0)
         
         self.firstDatePickView.layer.borderColor = borderColor.cgColor
@@ -143,7 +156,8 @@ class RegisterViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
         self.thirdTimePickInnerView.layer.masksToBounds = true
     }
     
-    
+    // toolBar에 함수를 사용할 땐 @objc를 사용하여 함수를 생성한다.
+    // toolBar에서 Done버튼이 눌렸을 때
     @objc func onPickDone() {
         self.mournerTextField.resignFirstResponder()
         self.infoTextField.resignFirstResponder()
@@ -157,11 +171,18 @@ class RegisterViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
         self.infoTextField.text = nil
     }
     
+    // 각각의 pickerView가 선택되었을 때 몇 개의 column이 표현될지 설정
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
+        if pickerView == self.picker1 {
+            return 2
+        } else {
+            return 1
+        }
     }
     
+    // 몇 개의 row가 표현될지
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        // 각각의 List에 저장된 데이터의 개수에 따라 설정
         if pickerView == self.picker1 {
             return mournerList.count
         } else {
